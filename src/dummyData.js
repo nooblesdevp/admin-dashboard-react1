e77294598f5c74341f2a2960d58007fa149fb66f
noobless <nuzulzen@gmail,com>
2021-09-04 18:17:34 +0700

    add dummyData user

---
+++ src/dummyData.js
@@ -0,0 +1,42 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 2000,
+  },
+  {
+    name: "Mar",
+    "Active User": 3000,
+  },
+  {
+    name: "Jun",
+    "Active User": 1000,
+  },
+  {
+    name: "Jul",
+    "Active User": 2000,
+  },
+  {
+    name: "Agu",
+    "Active User": 5000,
+  },
+  {
+    name: "Sep",
+    "Active User": 3000,
+  },
+  {
+    name: "Oct",
+    "Active User": 4000,
+  },
+  {
+    name: "Nov",
+    "Active User": 5000,
+  },
+  {
+    name: "Dec",
+    "Active User": 3000,
+  },
+];
